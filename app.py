56463f6079339c708f255c9862c4ecc660d4a34a
Oskar <oskar.schwarze@web.de>
2018-12-23 19:35:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@
 #Import necessary libraries
 
 import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
